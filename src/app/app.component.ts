56d5ac4e42f61e9a73de6ffeeaeffc3f715856e1
jhonldev <jhon.lopez.developer@gmail.com>
2024-06-02 17:04:55 -0400

    feat:portafolio completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, PortafolioComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portafolio';
 }
+
